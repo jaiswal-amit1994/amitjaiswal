cc0c342479b7d6006a83fa20e10db429d677d85f
Amit Jaiswal <jaiswalamit1994@gmail.com>
2024-11-04 20:26:55 +0530

    added

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,52 @@
-// import React from 'react'
+import React from 'react'
+import Link from 'next/link'
 
 export default function Footer() {
   return (
-    <div>footer</div>
+    <footer className="flex flex-col md:flex-row items-center gap-x-10 gap-y-4 py-20">
+      <div className="container mx-auto max-w-3xl justify-between">
+      <h3 className='title mb-4'>Connect</h3>
+      <div className="grid grid-cols-3 gap-4 w-full">
+        <div>
+            <ul className=''>
+              <li className=''>
+                <Link href={'/'}>Dribbble</Link>
+              </li>
+              <li className=''>
+                <Link href={'/'}>Youtube</Link>
+              </li>
+              <li className=''>
+                <Link href={'/'}>Linkedin</Link>
+              </li>
+            </ul>
+        </div>
+        <div>
+        <ul className=''>
+              <li className=''>
+                <Link href={'/'}>Instagram</Link>
+              </li>
+              <li className=''>
+                <Link href={'/'}>Facebook</Link>
+              </li>
+              <li className=''>
+                <Link href={'/'}>behance</Link>
+              </li>
+            </ul>
+        </div>
+        <div>
+        <ul className=''>
+              <li className=''>
+                <label className='block' htmlFor="">Email</label>
+                <Link href={'/'}>jaiswalamit1994@gmail.com</Link>
+              </li>
+              <li className=''>
+                <label className='block' htmlFor="">Phone</label>
+                <Link href={'/'}>+91 7607520136</Link>
+              </li>
+            </ul>
+        </div>
+      </div>
+      </div>
+    </footer>
   )
 }
